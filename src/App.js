21075ecaf3c88cfb387de27a24bdfca25122b6c2
Kunal Giradkar <33kunalg@gmail.com>
2022-01-30 08:47:50 +0530

    30th 30thjan

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -17,7 +17,7 @@ function App() {
         >
           Learn React
         </a>
-      <Headers/>
+      </header>
     </div>
   );
 }
